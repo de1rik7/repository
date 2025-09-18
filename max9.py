e70fc2d9b2fb58dac6a113099f8b61c5bbb0d15c
de1rik7 <mak.13579filatov@yandex.ru>
2025-09-18 11:18:06 +0300

    Delete max9.py

--- max9.py
+++
@@ -1,9 +0,0 @@
-def can_break_cholate():
-   n = int(input())
-   m = int(input())
-   k = int(input())
-   if (k % n == 0 or k % m == 0) and k <= n * m:
-     print("Да")
-   else: 
-     print("Нет")
-can_break_cholate()
